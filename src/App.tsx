7b6ebaf34f47709d515f91db6ca47f8e44ef80ec
GoldenGuts <jyotirmaygoldsmith@gmail.com>
2021-06-19 17:32:56 +0530

    Updated py script for fetching audio

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Component, useState, useRef, useEffect } from 'react';
 import './button.scss';
+import URLs from './testResults/FinalStreamableURLs.json';
 
 const App = () => {
 
@@ -21,13 +22,14 @@ const App = () => {
 //   });
 
   const startQuiz = async () => {
-    audio.src = "https://r6---sn-ci5gup-qxas.googlevideo.com/videoplayback?expire=1624014609&ei=sSrMYNPrOf74juMPz8uEuA4&ip=182.69.12.28&id=o-ABwmxflj-RbIvgCnFDX16J9VJEOL313y78vtlEYYcpyB&itag=251&source=youtube&requiressl=yes&mh=Ep&mm=31%2C26&mn=sn-ci5gup-qxas%2Csn-cvh7kney&ms=au%2Conr&mv=m&mvi=6&pl=21&gcr=in&initcwndbps=1168750&vprv=1&mime=audio%2Fwebm&ns=evoIdd3k66apGar0-qKUPYwF&gir=yes&clen=2894340&dur=170.081&lmt=1617040297132480&mt=1623991965&fvip=4&keepalive=yes&fexp=24001373%2C24007246&c=WEB&txp=5511222&n=Cb4wjSF4Tuc8vjq0R&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cgcr%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cdur%2Clmt&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRgIhAKpIhygVhsG5TVv--xCkQ8tWl-0kbjsNvljTYw503LKgAiEAt8m0gsWJKhiKebHkSkWMDCwtASEzoVVsfYHHmMVkOmw%3D&sig=AOq0QJ8wRAIgRxHDnHfdzQVfzYh8bdA27sXJ1nvFUiyy4mHel_pwxBUCIGobcdZUi-WqyTEPeGXgJEz1QIhByNd8polf0C8ftpiu";
+    audio.src = URLs.url1.toString();
     audio.load();
     
     var change = document.getElementById("btnText");
     // @ts-ignore: Object is possibly 'null'.
     if (change.innerHTML == "Start"){ // @ts-ignore: Object is possibly 'null'. 
       change.innerHTML = "Stop";
+      audio.currentTime = 120;
       audio.play();
     }
     // @ts-ignore: Object is possibly 'null'.
--- src/findingAudios.py
+++ src/findingAudios.py
@@ -1,6 +1,52 @@
 from __future__ import unicode_literals
 import youtube_dl
+from ytmusicapi import YTMusic
+import json
+import sys
 
-ydl_opts = {}
+ytmusic = YTMusic()
+search_results = ytmusic.search('One Direction', 'artists')
+  
+with open("testResults/YTMSearchResults.json", "w") as outfile:
+    outfile.write(json.dumps(search_results, indent = 4))
+    
+with open("testResults/YTMSearchResults.json", "r") as readfile:
+    YTMSearchData = json.load(readfile)
+    if YTMSearchData[0]['artist']=='One Direction':
+        channel = YTMSearchData[0]['browseId']
+        shuffle = YTMSearchData[0]['shuffleId']
+
+
+ydl_opts = {
+    'quiet': True,
+    'forceurl': True,
+    'format': 'bestaudio',
+    # 'logger': MyLogger(),
+    'skip_download': True,
+    'playliststart': 2,
+    'playlistend':  11,
+}
+
+sys.stdout = open("testResults/PythonOutput.txt", "w")
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
+    artistLink = 'https://music.youtube.com/watch?v=dQw4w9WgXcQ&list='+shuffle
+    search_results = ydl.download([artistLink])
+
+sys.stdout.close()
+
+fileName = 'testResults/PythonOutput.txt'
+finalDict ={}
+
+urlID = 1
+
+with open(fileName) as fh:
+    for line in fh:
+        description = line.strip().split(None, 1)
+        serial = 'url'+str(urlID)
+
+        finalDict[serial] = description
+        urlID = urlID + 1
+
+out_file = open("testResults/FinalStreamableURLs.json", "w")
+json.dump(finalDict, out_file, indent = 4, sort_keys = False)
+out_file.close()
